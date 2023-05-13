effe8aa8e7a2dd26545115ce862c68d2abcf24a0
Геннадий <@DESKTOP-P1KMKIH>
2023-05-13 13:24:13 +0300

    Добавьте файлы проекта.

---
+++ ClassDirectoryInfoHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class DirectoryInfoHelper
+    {
+        private DirectoryInfo? _di;
+        private long _totalSize;
+        private void GetSize(string path)
+        {
+            DirectoryInfo directoryInfo = new(path);
+            // Add files size:
+            IEnumerable<FileInfo> files = directoryInfo.GetFiles();
+            foreach (FileInfo fi in files)
+                _totalSize += fi.Length;
+            // Check inner folders:
+            IEnumerable<DirectoryInfo> folders = directoryInfo.GetDirectories();
+            foreach (DirectoryInfo di in folders)
+            {
+                try { GetSize(di.FullName); }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error with " + di.FullName + ": " + e.Message);
+                    continue;
+                }
+            }
+        }
+        public DirectoryInfoHelper(string path)
+        {
+            try
+            {
+                _di = new(path);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public long GetSize()
+        {
+            _totalSize = 0;
+            if (_di is not null)
+                GetSize(_di.FullName);
+            return _totalSize;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using System.Diagnostics.Metrics;
+using System.IO;
+// В задании требуется вывод на русском, потому весь вывод сделан на русском.
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        public static string InputString(string prompt, bool allowEmptyInput = false)
+        {
+            Console.Write(prompt);
+            string? result;
+            if (allowEmptyInput)
+                result = Console.ReadLine();
+            else
+                while ((result = Console.ReadLine()) is null || result == string.Empty)
+                    Console.Write(prompt);
+            return (result is not null ? result : string.Empty);
+        }
+        public static int fileCounter = 0;
+        public static int folderCounter = 0;
+        public static void ProcessFolder(DirectoryInfo directoryInfo)
+        {
+            IEnumerable<DirectoryInfo> folders = directoryInfo.GetDirectories();
+            if (folders.Any())
+            {
+                foreach (var di in folders)
+                {
+                    if (!di.Exists)
+                    {
+                        Console.WriteLine("Ошибка: папка " + di.FullName + " не существует");
+                        continue;
+                    }
+                    try { ProcessFolder(di); }
+                    catch (UnauthorizedAccessException)
+                    {
+                        Console.WriteLine("Ошибка: нет доступа к папке" + di.FullName);
+                    }
+                    finally { }
+                }
+            }
+            else
+            {
+                IEnumerable<FileInfo> files = directoryInfo.GetFiles();
+                foreach (var fi in files)
+                {
+                    if (!fi.Exists)
+                    {
+                        Console.WriteLine("Ошибка: файл " + fi.FullName + " не существует");
+                        continue;
+                    }
+                    try
+                    {
+                        DateTime fileCreatedDate = File.GetLastWriteTime(fi.FullName);
+                        TimeSpan difference = DateTime.Now.Subtract(fileCreatedDate);
+                        if (difference.TotalMinutes > 30)
+                        {
+                            File.Delete(fi.FullName);
+                            fileCounter++;
+                        }
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        Console.WriteLine("Ошикба: нет доступа к файлу " + fi.FullName);
+                    }
+                    finally { }
+                }
+                files = directoryInfo.GetFiles();
+                if (!files.Any())
+                {
+                    DateTime folderCreatedDate = Directory.GetLastWriteTime(directoryInfo.FullName);
+                    TimeSpan difference = DateTime.Now.Subtract(folderCreatedDate);
+                    if (difference.TotalMinutes > 30)
+                    {
+                        Directory.Delete(directoryInfo.FullName);
+                        folderCounter++;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            string path = (args.Length == 0 ?
+                        InputString("Выберите папку для удаления всего, что не использовалось больше 30 минут: ")
+                        : args[0]);
+            DirectoryInfoHelper helper = new(path);
+            long initialSize = helper.GetSize();
+            DirectoryInfo directoryInfo = new(path);
+            try
+            {
+                if (directoryInfo.Exists)
+                {
+                    ProcessFolder(directoryInfo);
+                }
+                else
+                    throw new Exception("Ошибка: папка не существует");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка: " + e.ToString());
+            }
+            finally { }
+            long finalSize = helper.GetSize();
+            Console.WriteLine("Исходный размер папки: " + initialSize.ToString() + " байт\n" +
+                              "Освобождено: " + (initialSize - finalSize).ToString() + " байт\n" +
+                              "Удалено " + fileCounter.ToString() + " файлов и " +
+                              folderCounter.ToString() + " папок\n" +
+                              "Текущий размер папки: " + finalSize.ToString() + " байт");
+        }
+    }
+}
